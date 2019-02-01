87cf6ac5bf54fc0f5e53da6f977dcca571bde9b9
Mark Jones <mark@uniregistry.com>
2019-02-01 09:32:53 -0500

    Minor fix for caching the _inspired_by in the StoryForm

--- diary/stories/views.py
+++ diary/stories/views.py
@@ -46,6 +46,7 @@ class CommonStoryFormMixin(ModelFormMixin):
         """ Return the story that inspired this story, take that from the object, or if creating a
               new story, from the GET params """
         if not hasattr(self, '_inspired_by'):
+            self._inspired_by = None
 
             if self.object:
                 self._inspired_by = self.object.inspired_by
@@ -56,6 +57,7 @@ class CommonStoryFormMixin(ModelFormMixin):
                 self._inspired_by = Story.objects.published(pk=int(pk)).first()
 
         return self._inspired_by
+
 
 
     def get_form_kwargs(self):
