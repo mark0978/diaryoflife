6af53f084b9b7dc3f9d244b681e538c304350c7d
Mark Jones <mark@uniregistry.com>
2019-02-12 14:08:22 -0500

    Added postmark email sending for signups.

--- diary/diary/production_settings.py
+++ diary/diary/production_settings.py
@@ -1,8 +1,9 @@
 """
 Production settings for diary project, these are not used with platform.sh
 """
+#import sys
 
-from settings import *
+from .settings import *
 
 import logging
 from logging import config as logging_config
@@ -31,9 +32,25 @@ logging_config.dictConfig({
             },
         'loggers': {
             '': {
+                #'handlers': ['stdout', ],
                 'handlers': ['sys-logger', ],
                 'level': logging.DEBUG,
                 'propagate': True,
                 },
             }
     })
+
+
+#EMAIL_BACKEND = 'email_log.backends.EmailBackend'
+#EMAIL_LOG_BACKEND = 'postmarker.django.EmailBackend'
+EMAIL_BACKEND = 'postmarker.django.EmailBackend'
+
+POSTMARK = {
+    "TOKEN": os.environ["DJANGO_POSTMARK_TOKEN"],
+    "TEST_MODE": False,
+    "VERBOSITY": 0,
+}
+
+EMAIL_SUBJECT_PREFIX = ""
+DEFAULT_FROM_EMAIL = "(Diary Of Life) <noreply@diaryof.life>"
+SERVER_EMAIL = "noreply@diaryof.life"
