40c31fd44c95fa6dc811ee92ae1b592cc4e0f757
boatcoder@gmail.com <Mark Jones>
2019-02-20 20:37:32 -0500

    Added can_edit to the Story object, removed the text object (for now)

--- diary/stories/serializers.py
+++ diary/stories/serializers.py
@@ -4,6 +4,15 @@ from stories.models import Story
 
 class StorySerializer(serializers.HyperlinkedModelSerializer):
 
+    url = serializers.HyperlinkedIdentityField(view_name="story-detail")
+    can_edit = serializers.SerializerMethodField()
+
     class Meta:
         model = Story
-        fields = ('title', 'tagline', 'author', 'text', 'html', 'inspired_by', 'published_at',)
+        fields = ('url', 'title', 'tagline', 'author', 'html', 'inspired_by', 
+                  'published_at', 'preceeded_by', 'next_chapter', 'can_edit')
+
+    def get_can_edit(self, obj):
+        """ Can the person that requested this object edit it?
+              (Are they the owner?) """
+        return self.context['request'].user == obj.author.user
